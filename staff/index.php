2832961dba6ad4cc47d5d938a537aaf748fccb7a
MohamedIbrahim1567 <89273415+MohamedIbrahim1567@users.noreply.github.com>
2021-08-20 20:40:25 +0200

    staff

---
+++ staff/index.php
@@ -0,0 +1,47 @@
+<?php include('includes/header.php')?>
+<?php include('../includes/session.php')?>
+<body>
+	<div class="pre-loader">
+		<div class="pre-loader-box">
+			<div class="loader-logo"><img src="../vendors/images/process-icon.jpg" alt=""></div>
+			<div class='loader-progress' id="progress_div">
+				<div class='bar' id='bar1'></div>
+			</div>
+			<div class='percent' id='percent1'>0%</div>
+			<div class="loading-text">
+				Loading...
+			</div>
+		</div>
+	</div>
+
+	<?php include('includes/navbar.php')?>
+
+	<?php include('includes/right_sidebar.php')?>
+
+	<?php include('includes/left_sidebar.php')?>
+
+	<div class="mobile-menu-overlay"></div>
+
+	<div class="main-container">
+		<div class="pd-ltr-20">
+			<div class="card-box pd-20 height-100-p mb-30">
+				<div class="row align-items-center">
+					<div class="col-md-4 user-icon">
+						<img src="../vendors/images/banner-img.png" alt="">
+					</div>
+					<div class="col-md-8">
+
+						<?php $query= mysqli_query($conn,"select * from tblemployees where emp_id = '$session_id'")or die(mysqli_error());
+								$row = mysqli_fetch_array($query);
+						?>
+
+						<h4 class="font-20 weight-500 mb-10 text-capitalize">
+							Welcome back <div class="weight-600 font-30 text-blue"><?php echo $row['FirstName']. " " .$row['LastName']; ?>,</div>
+						</h4>
+						<p class="font-18 max-width-600">you are in an institution established to serve the wider community.</p>
+					</div>
+				</div>
+			</div>
+	<?php include('includes/scripts.php')?>
+</body>
+</html>
